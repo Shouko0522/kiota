768c7eabfee091f1d6ad879fa830021a906fd220
Gavin Garza <gaving@live.com>
2020-02-18 21:37:38 +0900

    made an SQL query in the User model to join with property info and create viewed_properties

---
+++ app/controllers/property_viewings_controller.rb
@@ -0,0 +1,15 @@
+class PropertyViewingsController < ApplicationController
+  def feed
+    @property = current_user.feed_properties.first
+    
+  end
+
+  def create
+    current_user.property_viewings.create(status: params[:status])
+    redirect :feed
+  end
+
+  def show
+    @property_viewing = PropertyViewing.find(params[:id])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,19 @@ class User < ApplicationRecord
   # Renter
   has_many :property_viewings
   has_many :properties, through: :property_viewings
+
+  def feed_properties
+    to_be_seen_properties
+      .where('location >= ?', desired_location)
+      # .where()
+  end
+
+  def to_be_seen_properties
+    Property.joins(<<~SQL
+      LEFT JOIN property_viewings 
+        on properties.id = property_viewings.property_id 
+        and property_viewings.user_id = #{id}
+    SQL
+    ).where('property_viewings.id IS NULL')
+  end
 end
