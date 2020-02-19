055c0176ae6c1c4d43922c47ae327ec5e4fe9454
shouko oka <iamshk0522@gmail.com>
2020-02-19 20:03:27 +0900

    add photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,6 +26,14 @@ User.create(email:"yann@gmail.com",password:"123456")
 
 
 10.times do |index|
+  photos = [
+    'https://goodnaturestation.com/wp/wp-content/themes/biostyle/img/category/room/top-banner-background-room7.jpg',
+    'https://images.unsplash.com/photo-1486304873000-235643847519?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2378&q=80',
+    'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+    'https://images.unsplash.com/photo-1538183744096-546dd974a90e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80',
+    'https://images.unsplash.com/photo-1545454760-a8e55231441c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2551&q=80'
+  ]
+
     property = Property.new(
       information: INFO.sample,
       price: rand(50..100),
@@ -42,7 +50,7 @@ User.create(email:"yann@gmail.com",password:"123456")
       user: User.create(email: Faker::Internet.email, password: "123456"),
       deposit: rand(10..50)
     )
-    file = URI.open('https://goodnaturestation.com/wp/wp-content/themes/biostyle/img/category/room/top-banner-background-room7.jpg')
+    file = URI.open(photos.sample)
     property.photo.attach(io: file, filename: "room#{index}.jpg", content_type: 'image/jpg')
     property.save!
 end
