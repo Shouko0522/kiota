d5898e4c8e7c32bf552eac5756e3323874d588b9
shouko oka <iamshk0522@gmail.com>
2020-02-29 16:23:55 +0900

    add css

--- config/routes.rb
+++ config/routes.rb
@@ -31,13 +31,10 @@ Rails.application.routes.draw do
       resources :property do
         resources :reservations, only: [ :new, :create ]
       end
-
-    require "sidekiq/web"
-      authenticate :user, lambda { |u| u.admin } do
-        mount Sidekiq::Web => '/sidekiq'
-      end
-
-
     end
+      require "sidekiq/web"
+          authenticate :user, lambda { |u| u.admin } do
+            mount Sidekiq::Web => '/sidekiq'
+          end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
