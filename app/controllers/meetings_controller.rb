aa43e69bb12e8f74f704a3119fe9bc3d0ef974a1
shouko oka <iamshk0522@gmail.com>
2020-02-29 17:12:42 +0900

    fixed heroku

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -3,6 +3,7 @@ class MeetingsController < ApplicationController
   def index
     @applied_reservations = current_user.applied_reservations
     @confirmed_reservations = current_user.confirmed_reservations
-    ConfirmReservationJob.set(wait: 20.seconds).perform_later(@applied_reservations.sample.id)
+    ConfirmReservationJob.set(wait: 20.seconds).perform_later(@applied_reservations.sample.id) unless @applied_reservations.nil?
+
   end
 end
