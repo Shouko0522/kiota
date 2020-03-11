ee48c4fe2224d80c5f0b0243651b235d09a4d075
Gavin Garza <gaving@live.com>
2020-03-11 22:12:07 +0900

    added swipe feature

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
 import "bootstrap";
-import "controllers"
-import "../plugins/flatpickr"
+import "controllers";
+import "../plugins/flatpickr";
+import { initSwipe } from "../plugins/swipe";
+
+initSwipe();
