7ff3a6bad08e0f53667a6db6c06acef33bf21e69
Gavin Garza <gaving@live.com>
2020-03-12 19:14:24 +0900

    fixed criteria

--- app/controllers/property_viewings_controller.rb
+++ app/controllers/property_viewings_controller.rb
@@ -1,31 +1,7 @@
 class PropertyViewingsController < ApplicationController
   def feed
     @property = current_user.feed_properties.first
-    if @property.nil?
-      # redirect_to somewhere
-    else 
-      @property_viewing = PropertyViewing.new
-      @pets_tag = @property.pets == current_user.pets
-      @bar_tag = @property.bar_nearby == current_user.wants_bar_nearby
-      @cafe_tag = @property.cafe_nearby == current_user.wants_cafe_nearby
-
-      if current_user.wants_first_floor
-        @floor_tag = (@property.floor == 1)
-      end
-
-      if current_user.wants_supermarket_nearby
-        @supermarket_tag = (@property.distance_to_supermarket < 10)
-      end
-
-      if current_user.wants_park_nearby
-        @park_tag = (@property.distance_to_park < 10)
-      end
-      
-      if current_user.key_money
-        @zero_key_money_tag = (@property.key_money = 0)
-      end
-      
-    end
+    @property_viewing = PropertyViewing.new
   end
 
   def create
