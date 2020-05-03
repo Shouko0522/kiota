ff293a6f7aa7f97adff49287b216600a8364d235
gavingarza <gaving@live.com>
2020-05-03 15:30:25 +0900

    added sign out button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,11 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
+
+  # private
+  # def after_sign_out_path_for(resource_or_scope)
+  #   root_path
+  # end
 end
 
 def default_url_options
